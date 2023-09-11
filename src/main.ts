ff9115fdb32c601307a362bec9f0283c4a7cd751
Gabor Heja <gheja@uw830g8.git.stdio.hu>
2023-09-11 21:18:32 +0000

    Added Input

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 let _game: Game
 let _mapDom: HTMLDivElement
 let _gameObjects: Array<ObjBase> = []
+let _input: Input
 
 function init()
 {
     _mapDom = getElement("map") as HTMLDivElement
+    _input = new Input()
     _game = new Game()
     _game.init()
 }
