3b3676d0e47d9fbc24cc3e5cfa4a152c020ff248
Gabor Heja <gheja@uw830g8.git.stdio.hu>
2023-09-11 21:20:20 +0000

    ObjBase: renamed a tmp variable

--- src/obj_base.ts
+++ src/obj_base.ts
@@ -15,12 +15,12 @@ class ObjBase
 
     update()
     {
-        let tmp = _game.worldToScreenCoordinates(this.position)
+        let pos = _game.worldToScreenCoordinates(this.position)
         let width = _game.worldToScreenSize(this.graphics.canvas.width)
         let height = _game.worldToScreenSize(this.graphics.canvas.height)
-        
-        this.graphics.canvas.style.left = (tmp.x - width / 2) + "px"
-        this.graphics.canvas.style.top = (tmp.y - height / 2) + "px"
+
+        this.graphics.canvas.style.left = (pos.x - width / 2) + "px"
+        this.graphics.canvas.style.top = (pos.y - height / 2) + "px"
         this.graphics.canvas.style.width = width + "px"
         this.graphics.canvas.style.height = height + "px"
     }
