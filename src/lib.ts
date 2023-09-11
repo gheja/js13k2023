a562d3e1d62f7b0a8b12e7dd89300deac393bf71
Gabor Heja <gheja@uw830g8.git.stdio.hu>
2023-09-11 21:18:58 +0000

    lib: added clampn(), added lerp()

--- src/lib.ts
+++ src/lib.ts
@@ -12,3 +12,16 @@ function arrayPick(arr: Array<any>)
 {
     return arr[Math.floor(Math.random() * arr.length)]
 }
+
+// normalize and clamp
+function clampn(value: number, min: number, max: number)
+{
+    let a = (value - min) / (max - min)
+    a = Math.min(1, Math.max(0, a))
+    return a
+}
+
+function lerp(a: number, b: number, t: number)
+{
+    return a + (b - a) * t
+}
