6f53f114554a53805861f54f5f6941c54746e6f1
Gabor Heja <gheja@uw830g8.git.stdio.hu>
2023-09-12 23:34:27 +0000

    ObjCity now support labels

--- src/obj_base.ts
+++ src/obj_base.ts
@@ -4,14 +4,24 @@ class ObjBase
     public graphics: Renderer
     public visible: boolean
     public angle: number = 0 // in radians, 0 is right
+    public labelDom: HTMLDivElement
 
-    constructor(x: number, y: number, graphics: Renderer)
+    constructor(x: number, y: number, graphics: Renderer, label: string = null)
     {
         this.position = new Vec2D(x, y)
         this.graphics = graphics
         this.visible = true
+
         _mapDom.appendChild(graphics.canvas)
         _gameObjects.push(this)
+
+        if (label)
+        {
+            this.labelDom = document.createElement("div")
+            this.labelDom.innerHTML = label
+            this.labelDom.className = "city-label"
+            _mapDom.appendChild(this.labelDom)
+        }
     }
 
     update()
@@ -25,5 +35,12 @@ class ObjBase
         this.graphics.canvas.style.width = width + "px"
         this.graphics.canvas.style.height = height + "px"
         this.graphics.canvas.style.transform = "rotate(" + this.angle + "rad)"
+
+        if (this.labelDom)
+        {
+            this.labelDom.style.width = (_game.worldToScreenSize(400)) + "px"
+            this.labelDom.style.left = (pos.x - width / 2 - _game.worldToScreenSize(150)) + "px"
+            this.labelDom.style.top = (pos.y - height / 2 + _game.worldToScreenSize(90)) + "px"
+        }
     }
 }
